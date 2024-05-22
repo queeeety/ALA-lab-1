da1cf5e8f96248cb83687dff38851ff0fa64f2cf
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-05-22 20:08:58 +0300

    multiplyer function added and rotation added

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def VisualisatorOfTheVector(verts):
     # Display the figure
     plt.show()
     return 0
+
+
 def VisualisatorOfTheObject(verts, codes):
     # Create a path
     path = Path(verts, codes)
@@ -44,11 +46,17 @@ def VisualisatorOfTheObject(verts, codes):
     return 0
 
 
+def Multiplier(verts, i, j):
+    output = []
+    for vert in verts:
+         output.append((vert[0] * i.x + vert[1] * j.x, vert[0] * i.y + vert[1] * j.y))
+    return output
+
 
 def main():
     # Define the points of the free-shape object
     start_verts = [
-        (0.3, 0.1),  # left, bottom
+        (0.0, 0.0),  # left, bottom
         (0.1, 0.9),  # left, top
         (0.9, 0.9),  # right, top
         (0.9, 0.1),  # right, bottom
@@ -64,54 +72,50 @@ def main():
              Path.CLOSEPOLY,
              ]
 
-    VisualisatorOfTheObject(start_verts, start_codes)
+    VisualisatorOfTheVector(start_verts)
 
     i = Node(1, 0)
     j = Node(0, 1)
     print("The figures are displayed.")
-    print("""Choose the operation:
-    1. Rotation
-    2. Left rotation
-    3. Scaling
-    4. Mirroring
-    5. Tilt axes
-    6. Matrix tilt
-    """)
+    while True:
 
-    VisualisatorOfTheVector([(0, 0), (i.x, i.y)])
+        print("""Choose the operation:
+        1. Rotation
+        2. Left rotation
+        3. Scaling
+        4. Mirroring
+        5. Tilt axes
+        6. Matrix tilt
+        """)
+        operation = input("Enter the operation number: ")
+        match (operation):
+            case '1':
+                angle = int(input("Enter the angle of rotation (clockwise): "))
 
-    operation = input("Enter the operation number: ")
-    match (operation):
-        case '1':
-            angle = int(input("Enter the angle of rotation (clockwise): "))
+                # Rotate the vectors
+                new_i_x = i.x * np.cos(np.radians(angle)) - i.y * np.sin(np.radians(angle))
+                new_i_y = i.x * np.sin(np.radians(angle)) + i.y * np.cos(np.radians(angle))
+                i.x, i.y = new_i_x, new_i_y
 
-            # Rotate the vectors
-            new_i_x = i.x * np.cos(np.radians(angle)) - i.y * np.sin(np.radians(angle))
-            new_i_y = i.x * np.sin(np.radians(angle)) + i.y * np.cos(np.radians(angle))
-            i.x, i.y = new_i_x, new_i_y
+                new_j_x = j.x * np.cos(np.radians(angle)) - j.y * np.sin(np.radians(angle))
+                new_j_y = j.x * np.sin(np.radians(angle)) + j.y * np.cos(np.radians(angle))
+                j.x, j.y = new_j_x, new_j_y
 
-            new_j_x = j.x * np.cos(np.radians(angle)) - j.y * np.sin(np.radians(angle))
-            new_j_y = j.x * np.sin(np.radians(angle)) + j.y * np.cos(np.radians(angle))
-            j.x, j.y = new_j_x, new_j_y
+                new_verts = Multiplier(start_verts, i, j)
+                VisualisatorOfTheVector(new_verts)
 
-            VisualisatorOfTheVector([(0, 0), (i.x, i.y)])
-            VisualisatorOfTheVector([(0, 0), (j.x, j.y)])
-
-            print(i.x == i.y)
-            print(j.x == j.y)
-            print(i.x == np.sqrt(2))
-        # case '2':
-        #
-        # case '3':
-        #
-        # case '4':
-        #
-        # case '5':
-        #
-        # case '6':
-        #
-        # case _:
-        #     print("Incorrect operation number.")
+            # case '2':
+            #
+            # case '3':
+            #
+            # case '4':
+            #
+            # case '5':
+            #
+            # case '6':
+            #
+            # case _:
+            #     print("Incorrect operation number.")
 
 
 
