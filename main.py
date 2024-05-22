26bca30eccf831af34610b87e1c7bccf3a0de048
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-05-22 15:52:51 +0300

    added main vectors rotation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
-
+from math import cos, sin
 import operations as op
 
 
@@ -16,10 +16,38 @@ class Node:
         self.x = x
         self.y = y
 
+def VisualisatorOfTheVector(verts):
+    # Unzip the vertices into x and y coordinates
+    x, y = zip(*verts)
+    # Add the first point to the end to close the polygon
+    x = x + (x[0],)
+    y = y + (y[0],)
+    # Plot the polygon
+    plt.plot(x, y, 'r-')
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+    # Display the figure
+    plt.show()
+    return 0
+def VisualisatorOfTheObject(verts, codes):
+    # Create a path
+    path = Path(verts, codes)
+    # Create a patch
+    patch = patches.PathPatch(path, facecolor='red', lw=1)
+    # Add the patch to the Axes
+    ax.add_patch(patch)
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+
+    # Display the figure
+    plt.show()
+    return 0
+
+
 
 def main():
     # Define the points of the free-shape object
-    verts = [
+    start_verts = [
         (0.3, 0.1),  # left, bottom
         (0.1, 0.9),  # left, top
         (0.9, 0.9),  # right, top
@@ -28,24 +56,66 @@ def main():
         (0.3, 0.1),
     ]
     # Define the path codes
-    codes = [Path.MOVETO,
+    start_codes = [Path.MOVETO,
              Path.LINETO,
              Path.LINETO,
              Path.LINETO,
              Path.LINETO,
              Path.CLOSEPOLY,
              ]
-    # Create a path
-    path = Path(verts, codes)
-    # Create a patch
-    patch = patches.PathPatch(path, facecolor='red', lw=1)
-    # Add the patch to the Axes
-    ax.add_patch(patch)
-    plt.xlim([0, 1])
-    plt.ylim([0, 1])
 
-    # Display the figure
-    plt.show()
+    VisualisatorOfTheObject(start_verts, start_codes)
+
+    i = Node(1, 0)
+    j = Node(0, 1)
+    print("The figures are displayed.")
+    print("""Choose the operation:
+    1. Rotation
+    2. Left rotation
+    3. Scaling
+    4. Mirroring
+    5. Tilt axes
+    6. Matrix tilt
+    """)
+
+    VisualisatorOfTheVector([(0, 0), (i.x, i.y)])
+
+    operation = input("Enter the operation number: ")
+    match (operation):
+        case '1':
+            angle = int(input("Enter the angle of rotation (clockwise): "))
+
+            # Rotate the vectors
+            new_i_x = i.x * np.cos(np.radians(angle)) - i.y * np.sin(np.radians(angle))
+            new_i_y = i.x * np.sin(np.radians(angle)) + i.y * np.cos(np.radians(angle))
+            i.x, i.y = new_i_x, new_i_y
+
+            new_j_x = j.x * np.cos(np.radians(angle)) - j.y * np.sin(np.radians(angle))
+            new_j_y = j.x * np.sin(np.radians(angle)) + j.y * np.cos(np.radians(angle))
+            j.x, j.y = new_j_x, new_j_y
+
+            VisualisatorOfTheVector([(0, 0), (i.x, i.y)])
+            VisualisatorOfTheVector([(0, 0), (j.x, j.y)])
+
+            print(i.x == i.y)
+            print(j.x == j.y)
+            print(i.x == np.sqrt(2))
+        # case '2':
+        #
+        # case '3':
+        #
+        # case '4':
+        #
+        # case '5':
+        #
+        # case '6':
+        #
+        # case _:
+        #     print("Incorrect operation number.")
+
+
+
+
 
 if __name__ == "__main__":
     main()
